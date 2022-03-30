f4a6e0f39f343edbae469fff45f6de45e79addb9
Tolu Adesina <tolu.adesina79@gmail.com>
2022-03-30 22:32:07 +0200

    ADD dynamic navigational comp for development

--- src/shared/components/navigation/navi.tsx
+++ src/shared/components/navigation/navi.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import CN from "classnames";
 import { Outlet, Link } from "react-router-dom";
 import SCSS from "./navi.module.scss";
 
 export default function Navi() {
 
-  const naviClassName = CN("flex flex-col fixed top-4 left-4 md:top-10 md:left-10 z-10  bg-slate-300 rounded-md");
-  const naviButtonClassName = CN("px-3 py-1 text-black rounded-md m-2 bg-white font-bold text-center");
+  const [nav, setNav] = useState<boolean>(false);
+
+  const naviComponentClassName = CN(SCSS["navi--aside"])
+  const naviClassName = CN(SCSS["navi--navigation"], "flex flex-col bg-slate-300 rounded-md");
+  const naviButtonClassName = CN("px-3 py-1 text-black rounded-md mx-3 my-2 bg-white font-bold text-center");
+  const naviIndicatorClassName = CN(SCSS["navi--inidcator"], "bg-slate-300");
+  const naviIndicatorTextClassName = CN("text-center text-base font-black text-white m-0 px-2 py-1")
 
   return (
-    <aside>
+    <aside className={naviComponentClassName.concat(` ${nav ? SCSS["active"] : ""}`)}>
       <nav className={naviClassName}>
-        <Link className={naviButtonClassName} to="/discovery">Discovery</Link>
-        <Link className={naviButtonClassName} to="/map">Map</Link>
-        <Link className={naviButtonClassName} to="/">Index</Link>
+        {
+          nav && (
+            <>
+              <Link className={naviButtonClassName} to="/discovery">Discovery</Link>
+              <Link className={naviButtonClassName} to="/map">Map</Link>
+              <Link className={naviButtonClassName} to="/">Index</Link>
+            </>
+          )
+        }
+
+        {
+          (!nav) && <button className={naviIndicatorClassName} onClick={() => setNav(true)}>
+            <p className={naviIndicatorTextClassName}>X</p>
+          </button>
+        }
+        {
+          (nav) && <button className={naviIndicatorClassName.concat(` ${SCSS["active"]}`)} onClick={() => setNav(false)}>
+            <p className={naviIndicatorTextClassName}>O</p>
+          </button>
+        }
       </nav>
+
+
       <Outlet />
     </aside>
   )
